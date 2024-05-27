e8a42b46420fcc25761107fc69ed81682edd0334
Anuragt1104 <bb1221373@dbeb.iitd.ac.in>
2024-05-27 23:17:17 +0530

    ling learning done

--- lib/presentation/ling_learning/ling_learning_screen.dart
+++ lib/presentation/ling_learning/ling_learning_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 import 'package:svar_new/presentation/ling_learning/ling_learning_provider.dart';
 import 'package:svar_new/widgets/custom_button.dart';
-
-import 'package:flutter/material.dart';
 import 'package:svar_new/core/app_export.dart';
 
 class LingLearningScreen extends StatefulWidget {
@@ -36,69 +36,58 @@ class LingLearningScreenState extends State<LingLearningScreen> {
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(ImageConstant.imgGroup7),
-                  fit: BoxFit.cover)),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
+            image: DecorationImage(
+              image: AssetImage(ImageConstant.imgGroup7),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Stack(
             children: [
-              _buildAppBar(context),
-              Stack(children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 18.h,
-                          vertical: 36.v,
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              lingLearningProvider.selectedCharacter,
-                              style: theme.textTheme.displayLarge!
-                                  .copyWith(fontSize: 45),
-                            ),
-                            SizedBox(height: 27.v),
-                            GestureDetector(
-                              child: CustomButton(
-                                  type: ButtonType.Next,
-                                  onPressed: () {
-                                    NavigatorService.pushNamed(
-                                      AppRoutes.lingLearningQuickTipScreen,
-                                    );
-                                  }),
-                            ),
-                            SizedBox(height: 19.v)
-                          ],
-                        ),
-                      ),
-                    ],
+              Column(
+                children: [
+                  _buildAppBar(context),
+                ],
+              ),
+              Positioned(
+                  left: 120,
+                  bottom: 60,
+                  child: Container(
+                    height: 50,
+                    width: 100,
+                    child: CustomButton(
+                      type: ButtonType.Next,
+                      onPressed: () {
+                        NavigatorService.pushNamed(
+                          AppRoutes.lingLearningQuickTipScreen,
+                        );
+                      },
+                    ),
+                  )),
+              Positioned(
+                right: 80,
+                bottom: 0,
+                child: Container(
+                  height: 360,
+                  width: 270,
+                  child: Image.asset(
+                    ImageConstant.imgProtaganist1,
+                    fit: BoxFit.contain,
                   ),
                 ),
-              ]),
-              Align(
-                alignment: Alignment.bottomRight, // Adjust this as needed
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      right: 20.h, bottom: 5.h), // Adjust padding as needed
-                  // child: CustomImageView(
-                  //   imagePath: ImageConstant.imgProtaganist1,
-                  //   height: 420.v,
-                  //   width: 200.h,
-                  // ),
-                ),
               ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      right: 1.h, bottom: 3.h), // Adjust padding as needed
-                  child: CustomButton(type: ButtonType.Tip, onPressed: () {}),
+              Positioned(
+                right: 10,
+                bottom: 50,
+
+                child:Container(
+                  height: 70,
+                  width: 100,
+                  child: CustomButton(
+                    type: ButtonType.Tip,
+                    onPressed: () {},
+                  ),
                 ),
+                
               ),
             ],
           ),
@@ -107,59 +96,32 @@ class LingLearningScreenState extends State<LingLearningScreen> {
     );
   }
 
-  /// Section Widget
   Widget _buildAppBar(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(
-        left: 11.h,
-        right: 6.h,
-      ),
+      padding: EdgeInsets.symmetric(horizontal: 30,vertical: 30),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: EdgeInsets.only(left: 1.h),
-            child: CustomButton(type: ButtonType.Back, onPressed: () {}),
+          CustomButton(
+            type: ButtonType.Back,
+            onPressed: () {},
           ),
           Spacer(),
-          Padding(
-            padding: EdgeInsets.only(left: 1.h),
-            child: CustomButton(
-                type: ButtonType.Home,
-                onPressed: () {
-                  NavigatorService.pushNamed(
-                    AppRoutes.welcomeScreenPotraitScreen,
-                  );
-                }),
+          CustomButton(
+            type: ButtonType.Replay,
+            onPressed: () {
+              NavigatorService.pushNamed(
+                AppRoutes.welcomeScreenPotraitScreen,
+              );
+            },
           ),
-          SizedBox(width: 5),
-          Padding(
-            padding: EdgeInsets.only(left: 1.h),
-            child: CustomButton(type: ButtonType.FullVolume, onPressed: () {}),
-          )
+           SizedBox(width: 5,),
+          CustomButton(
+            type: ButtonType.FullVolume,
+            onPressed: () {},
+          ),
         ],
       ),
-    );
-  }
-
-  /// Navigates to the phonemsLevelScreenTwoScreen when the action is triggered.
-  onTapBtnArrowDown(BuildContext context) {
-    NavigatorService.pushNamed(
-      AppRoutes.phonemsLevelScreenTwoScreen,
-    );
-  }
-
-  /// Navigates to the lingLearningQuickTipScreen when the action is triggered.
-  onTapNextBTNTextButton(BuildContext context) {
-    NavigatorService.pushNamed(
-      AppRoutes.lingLearningQuickTipScreen,
-    );
-  }
-
-  /// Navigates to the lingLearningQuickTipScreen when the action is triggered.
-  onTapImgLaptop(BuildContext context) {
-    NavigatorService.pushNamed(
-      AppRoutes.lingLearningQuickTipScreen,
     );
   }
 }
