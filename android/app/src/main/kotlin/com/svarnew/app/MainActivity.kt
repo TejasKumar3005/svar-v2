d4970e4bb0e512269691c7100fd11e4f7edc9c1f
adityaraj-09 <itsrishabhforyou@gmail.com>
2024-06-10 10:55:34 +0530

    Update Java and Kotlin compatibility versions, and remove commented code in build.gradle and AndroidManifest.xml

--- android/app/src/main/kotlin/com/svarnew/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/svarnew/app/MainActivity.kt
@@ -10,40 +10,7 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
-    private val CHANNEL = "parental_control_channel"
-
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method == "checkParentalControlPIN") {
-                val enteredPin: String? = call.argument("pin")
-                val parentalControlEnabled = isParentalControlsEnabled(this)
-                var pinMatches = false
-                if (parentalControlEnabled) {
-                    pinMatches = checkPINWithDeviceSettings(this, enteredPin)
-                }
-                result.success(pinMatches)
-            } else {
-                result.notImplemented()
-            }
-        }
-    }
-
-    private fun isParentalControlsEnabled(context: Context): Boolean {
-        val devicePolicyManager = context.getSystemService(Context.DEVICE_POLICY_SERVICE) as? DevicePolicyManager
-        val userManager = context.getSystemService(Context.USER_SERVICE) as? UserManager
-        return if (devicePolicyManager != null && userManager != null) {
-            val adminComponent = ComponentName(context, MyDeviceAdminReceiver::class.java)
-            devicePolicyManager.isProfileOwnerApp(context.packageName) && userManager.hasUserRestriction(UserManager.DISALLOW_INSTALL_APPS)
-        } else {
-            false
-        }
-    }
-
-    private fun checkPINWithDeviceSettings(context: Context, enteredPin: String?): Boolean {
-        val parentalControlPIN = Settings.Secure.getString(context.contentResolver, "parental_control_pin")
-        return !parentalControlPIN.isNullOrEmpty() && parentalControlPIN == enteredPin
-    }
+    
 }
 
 
