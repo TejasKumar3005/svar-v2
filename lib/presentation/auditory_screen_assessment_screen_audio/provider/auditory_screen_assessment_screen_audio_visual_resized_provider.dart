6d42651de980fc82f27752d2c39b7b0d724b753b
Coder1010ayush <coderayush1010@gmail.com>
2024-06-18 22:48:35 +0530

    type fixed

--- lib/presentation/auditory_screen_assessment_screen_audio/provider/auditory_screen_assessment_screen_audio_visual_resized_provider.dart
+++ lib/presentation/auditory_screen_assessment_screen_audio/provider/auditory_screen_assessment_screen_audio_visual_resized_provider.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../../../core/app_export.dart';
 import '../models/auditory_screen_assessment_screen_audio_visual_resized_model.dart';
@@ -14,6 +16,29 @@ AuditoryScreenAssessmentScreenAudioVisualResizedModel
 auditoryScreenAssessmentScreenAudioVisualResizedModelObj =
 AuditoryScreenAssessmentScreenAudioVisualResizedModel();
 
+
+  Future<void> incrementLevelCount() async {
+    try {
+      String? uid = FirebaseAuth.instance.currentUser?.uid;
+      DocumentReference userRef =
+          FirebaseFirestore.instance.collection('users').doc(uid);
+      await FirebaseFirestore.instance.runTransaction((transaction) async {
+        DocumentSnapshot snapshot = await transaction.get(userRef);
+        if (snapshot.exists) {
+          int currentLevelCount = (snapshot.data()
+              as Map<String, dynamic>?)?['phoneme_current_level'];
+          int newLevelCount = currentLevelCount + 1;
+          transaction.update(userRef, {'phoneme_current_level': newLevelCount});
+        } else {
+          throw Exception('User not found!');
+        }
+      });
+
+      print('levelCount incremented successfully!');
+    } catch (e) {
+      print('Error incrementing levelCount: $e');
+    }
+  }
 @override
 void dispose() {
 super.dispose();
